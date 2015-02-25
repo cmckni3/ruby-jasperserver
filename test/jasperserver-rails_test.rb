14fd8af3178d1e14efedf19106fa14f5d2c92c04
Chris McKnight <cmckni3@gmail.com>
2015-02-24 23:22:45 -0600

    Cleans up tests and uses ruby 1.9 hash syntax

--- test/jasperserver-rails_test.rb
+++ test/jasperserver-rails_test.rb
@@ -5,18 +5,18 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
   c.hook_into :webmock
-  c.default_cassette_options = { :record => :new_episodes, :erb => true }
+  c.default_cassette_options = { record: :new_episodes, erb: true }
   c.filter_sensitive_data('<USERNAME>') { Rails.configuration.jasperserver[Rails.env.to_sym][:username] }
   c.filter_sensitive_data('<PASSWORD>') { Rails.configuration.jasperserver[Rails.env.to_sym][:password] }
   c.filter_sensitive_data('<URL>') { Rails.configuration.jasperserver[Rails.env.to_sym][:url] }
   c.filter_sensitive_data('<SET-COOKIE>') do |interaction|
-    interaction.response.headers["Set-Cookie"].first if !interaction.response.headers["Set-Cookie"].nil?
+    interaction.response.headers['Set-Cookie'].first if interaction.response.headers['Set-Cookie']
   end
   c.filter_sensitive_data('<COOKIE>') do |interaction|
-    interaction.request.headers["Cookie"].first if !interaction.request.headers["Cookie"].nil?
+    interaction.request.headers['Cookie'].first if interaction.request.headers['Cookie']
   end
   c.filter_sensitive_data('<SERVER>') do |interaction|
-    interaction.response.headers["Server"].first if !interaction.response.headers["Server"].nil?
+    interaction.response.headers['Server'].first if interaction.response.headers['Server']
   end
 end
 
@@ -26,7 +26,7 @@ class JasperserverRailsTest < ActiveSupport::TestCase
       JasperserverRails::Jasperserver.new do
         format 'pdf'
         report 'samples/Department'
-        params({ :Value1 => 'Value1', :Value2 => 'Value2' })
+        params({ Value1: 'Value1', Value2: 'Value2' })
         run_report('test/dummy/tmp/reports/test.pdf')
       end
     end
@@ -38,7 +38,7 @@ class JasperserverRailsTest < ActiveSupport::TestCase
       JasperserverRails::Jasperserver.new do
         format 'pdf'
         report 'samples/Department'
-        params({ :Value1 => 'Value1' })
+        params({ Value1: 'Value1' })
         run_report('test/dummy/tmp/reports/test2.pdf')
       end
     end
@@ -50,7 +50,7 @@ class JasperserverRailsTest < ActiveSupport::TestCase
       JasperserverRails::Jasperserver.new.run_report 'test/dummy/tmp/reports/test3.pdf' do
         format 'pdf'
         report 'samples/Department'
-        params({ :Value1 => 'Value1' })
+        params({ Value1: 'Value1' })
       end
     end
     File.exists?('test/dummy/tmp/reports/test3.pdf')
@@ -61,7 +61,7 @@ class JasperserverRailsTest < ActiveSupport::TestCase
       JasperserverRails::Jasperserver.new.run_report 'test/dummy/tmp/reports/test4.pdf' do
         format 'pdf'
         report 'samples/Department'
-        params({ :Value1 => 'Value1' })
+        params({ Value1: 'Value1' })
       end
     end
     File.exists?('test/dummy/tmp/reports/test4.pdf')
@@ -70,7 +70,7 @@ class JasperserverRailsTest < ActiveSupport::TestCase
       JasperserverRails::Jasperserver.new.run_report 'test/dummy/tmp/reports/test5.pdf' do
         format 'pdf'
         report 'samples/Department'
-        params({ :Value1 => 'Value1' })
+        params({ Value1: 'Value1' })
       end
     end
     File.exists?('test/dummy/tmp/reports/test5.pdf')
