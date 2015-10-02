3b23968645861b38ee222ceef82432d5e6fe9171
Chris McKnight <cmckni3@gmail.com>
2015-10-02 12:55:13 -0500

    * Removes hardcoded reports/reports path * Fixes tests

--- test/dummy/config/initializers/jasperserver.rb
+++ test/dummy/config/initializers/jasperserver.rb
@@ -1,3 +1,9 @@
 Rails.configuration.after_initialize do
-  Rails.configuration.jasperserver = { test: { url: 'http://localhost:3000', username: 'username', password: 'password' } }
+  Rails.configuration.jasperserver = {
+    test: {
+      url: 'http://localhost:3000',
+      username: 'username',
+      password: 'password'
+    }
+  }
 end
